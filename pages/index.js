bf0687327befbe407520e7ae1958cf1c67a4418b
Peng Chen <pchen0816@outlook.com>
2020-07-14 23:23:34 -0700

    receipe-page done

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,38 @@ import Carousel from '../components/home/Carousel';
 import Daily_meal from '../components/home/Daily_meal';
 
 
-const Home = () => (
+const Home = (props) => (
     <Layout>
         <Carousel/>
-        <Daily_meal/>
+        <Daily_meal data={props}/>
     </Layout>
 
 )
 
-// export async function getServerSideProps (context){
+export async function getServerSideProps (context){
 
-//     const url = '';
-//     const response = await fetch(url);
-//     const data = await response.json();
+  // const url = 'https://foodie-journals.herokuapp.com/api/receipes/breakfast/';
+  const url_breakfast = "http://127.0.0.1:8000/api/receipes/breakfast";
+  const response_breakfast = await fetch(url_breakfast);
+  const data_breakfast = await response_breakfast.json();
 
-//     return {
-//         props: {
-//             data : data,
-//         },
-//     }
-// }
+  const url_lunch = "http://127.0.0.1:8000/api/receipes/lunch/"
+  const response_lunch = await fetch(url_lunch);
+  const data_lunch = await response_lunch.json();
+
+  const url_dinner = "http://127.0.0.1:8000/api/receipes/dinner/"
+  const response_dinner = await fetch(url_dinner);
+  const data_dinner = await response_dinner.json();
+
+  return {
+    props: {
+      breakfast: data_breakfast,
+      lunch: data_lunch,
+      dinner: data_dinner,
+    }
+  };
+  
+}
+
 
 export default Home;
--- pages/receipes.js
+++ pages/receipes.js
@@ -1,26 +1,30 @@
 import Layout from '../components/layout/Layout';
 import Filter from '../components/receipes/Filter';
+import All_receipes from '../components/receipes/All_receipes'
 
 
-const Receipes = () => (
+const Receipes = (props) => {
+  // for (let i = 0; i<props.length; i++)
+  
+  return (
     <Layout>
         <Filter/>
-        {/* <Daily_meal/> */}
+        <All_receipes data={props.data}/>
     </Layout>
 
-)
+)}
 
-// export async function getServerSideProps (context){
+export async function getServerSideProps (context){
 
-//     const url = '';
-//     const response = await fetch(url);
-//     const data = await response.json();
+    const url = 'http://127.0.0.1:8000/api/receipes';
+    const response = await fetch(url);
+    const data = await response.json();
 
-//     return {
-//         props: {
-//             data : data,
-//         },
-//     }
-// }
+    return {
+        props: {
+            data : data,
+        },
+    }
+}
 
 export default Receipes;
