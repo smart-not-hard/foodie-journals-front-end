137afbc5ded70b8a52f5a22bf4dea60748f38586
Peng Chen <pchen0816@outlook.com>
2020-07-19 22:35:16 -0700

    backend link updated

--- components/url_back.js
+++ components/url_back.js
@@ -3,8 +3,8 @@
 
 
 //test
-module.exports = "http://127.0.0.1:8001/"
+// module.exports = "http://127.0.0.1:8001/"
 
 
 // my backend
-//module.exports = "https://foodie-journals.herokuapp.com/api/recipes/"
+module.exports = "https://foodie-journals.herokuapp.com/api/recipes/"
